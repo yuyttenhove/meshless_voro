01a2210cb0cbc0605624488af6b754ec32af5f6b
Yolan Uyttenhove <yolan.uyttenhove@ugent.be>
2023-03-01 14:45:04 +0100

    improve plotting

--- plot_vortess.py
+++ plot_vortess.py
@@ -12,22 +12,38 @@ def read_faces(fname: Path) -> np.ndarray:
     return np.stack([start, end], axis=1)
 
 
-def plot(faces: np.ndarray):
-    lines = LineCollection(faces, color="r")
+def read_generators(fname: Path) -> np.ndarray:
+    data = h5py.File(fname, "r")
+    return data["Cells/Generator"][:]
+
+def read_centroids(fname: Path) -> np.ndarray:
+    data = h5py.File(fname, "r")
+    return data["Cells/Centroid"][:]
+
+
+def plot(faces: np.ndarray, generators = None, centroids = None):
+    lines = LineCollection(faces, color="r", lw=1)
     fig, ax = plt.subplots(figsize=(6, 6))
     ax.add_collection(lines)
     ax.set_aspect("equal")
     ax.set_xlim(min([f[:, 0].min() for f in faces]), max([f[:, 0].max() for f in faces]))
     ax.set_ylim(min([f[:, 1].min() for f in faces]), max([f[:, 1].max() for f in faces]))
+    if generators is not None:
+        ax.scatter(generators[:, 0], generators[:, 1], c="green", s=0.5)
+    if centroids is not None:
+        ax.scatter(centroids[:, 0], centroids[:, 1], c="blue", s=0.5)
     ax.axis("off")
     fig.tight_layout()
     fig.savefig("test.png", dpi=300)
 
 
-def main():
-    faces = read_faces("test.hdf5")
-    plot(faces)
+def main(fname):
+    plot(
+        faces = read_faces(fname),
+        generators = read_generators(fname),
+        centroids = read_centroids(fname),
+    )
 
 
 if __name__ == "__main__":
-    main()
+    main("test.hdf5")
