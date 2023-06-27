948b0fc6a85700d544ac26b0769e5cf4048a4004
Yolan Uyttenhove <yolan.uyttenhove@ugent.be>
2023-06-27 15:03:45 +0200

    new version

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! An implementation of the [Meshless Voronoi algorithm](https://hal.inria.fr/hal-01927559/file/voroGPU.pdf) in rust.
+//! **An implementation of the [Meshless Voronoi algorithm](https://hal.inria.fr/hal-01927559/file/voroGPU.pdf) in rust.**
 //!
 //! The algorithm is primarily aimed at generating 3D Voronoi diagrams, but can also be used to compute 1D and 2D Voronoi diagrams.
 //! Like Voro++, this algorithm is _meshless_ implying that no global geometry is constructed. Instead a cell based approach is used and we only compute integrals (cell/face volumes and centroids) and connectivity information (it is possible to determine a cell's neighbours).
@@ -8,6 +8,12 @@
 //! If necessary, arbitrary precision arithmetic is used to treat degeneracies and to ensure globaly consistent local geometry, see the appendix of [this reference](https://hal.inria.fr/hal-01927559/file/voroGPU.pdf) for more info:
 //! > <cite>Nicolas Ray, Dmitry Sokolov, Sylvain Lefebvre, Bruno Lévy. Meshless Voronoi on the GPU. ACM
 //! > Transactions on Graphics, 2018, 37 (6), pp.1-12.  10.1145/3272127.3275092 .  hal-01927559<cite>
+//! 
+//! **Features**:
+//! - Construction of 1D, 2D and 3D Voronoi grids.
+//! - Save Voronoi grid to hdf5 format (requires `hdf5` feature)
+//! - Parallel construction of the voronoi grid (requires `rayon` feature)
+//! - Evaluation of custom _integrals_ for cells (e.g. weighted centroid) and faces (e.g. solid angles).
 
 mod bounding_sphere;
 mod geometry;
