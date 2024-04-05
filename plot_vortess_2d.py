8c6ae349ca174aa2a59f7bccfca4e0ee418b99a5
Yolan Uyttenhove <yolan.uyttenhove@ugent.be>
2024-04-05 15:13:27 +0200

    Formatted python file

--- plot_vortess_2d.py
+++ plot_vortess_2d.py
@@ -1,9 +1,9 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
 from pathlib import Path
 import h5py
+
 
 def read_faces(fname: Path) -> np.ndarray:
     data = h5py.File(fname, "r")
@@ -11,21 +11,28 @@ def read_faces(fname: Path) -> np.ndarray:
     end = data["Faces/End"][:][:, :2]
     return np.stack([start, end], axis=1)
 
+
 def read_generators(fname: Path) -> np.ndarray:
     data = h5py.File(fname, "r")
     return data["Cells/Generator"][:]
+
 
 def read_centroids(fname: Path) -> np.ndarray:
     data = h5py.File(fname, "r")
     return data["Cells/Centroid"][:]
 
-def plot(faces: np.ndarray, generators = None, centroids = None):
+
+def plot(faces: np.ndarray, generators=None, centroids=None):
     lines = LineCollection(faces, color="r", lw=1)
     fig, ax = plt.subplots(figsize=(6, 6))
     ax.add_collection(lines)
     ax.set_aspect("equal")
-    ax.set_xlim(min([f[:, 0].min() for f in faces]), max([f[:, 0].max() for f in faces]))
-    ax.set_ylim(min([f[:, 1].min() for f in faces]), max([f[:, 1].max() for f in faces]))
+    ax.set_xlim(
+        min([f[:, 0].min() for f in faces]), max([f[:, 0].max() for f in faces])
+    )
+    ax.set_ylim(
+        min([f[:, 1].min() for f in faces]), max([f[:, 1].max() for f in faces])
+    )
     if generators is not None:
         ax.scatter(generators[:, 0], generators[:, 1], c="green", s=0.5)
     if centroids is not None:
@@ -34,12 +41,14 @@ def plot(faces: np.ndarray, generators = None, centroids = None):
     fig.tight_layout()
     fig.savefig("test.png", dpi=300)
 
+
 def main(fname):
     plot(
-        faces = read_faces(fname),
-        generators = read_generators(fname),
-        centroids = read_centroids(fname),
+        faces=read_faces(fname),
+        generators=read_generators(fname),
+        centroids=read_centroids(fname),
     )
+
 
 if __name__ == "__main__":
     main("test_2_d.hdf5")
